11395d86b1cfa0036517c3a979b3e0ef39a8f545
Shaukatjassme <shaukat.jassme786@gmail.com>
2023-08-25 23:18:33 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -4,25 +4,46 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
+import wrongAudio from './wrong.mp3'; // Import the audio file
+
 export default function App() {
   const [selectedOption, setSelectedOption] = useState(null);
   const correctAnswer = 'Pakistan';
   const isAnswerCorrect = selectedOption === correctAnswer;
   const isWrongSelection = selectedOption !== null && !isAnswerCorrect;
-  const isFirstQuestion = false; // Set this to true for the first question
+  const isFirstQuestion = true; // Set this to true for the first question
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0); // Keep track of current question index
 
   const handleOptionClick = (event, country) => {
     event.preventDefault();
     setSelectedOption(country);
+
+    // Play wrong audio if the answer is wrong
+    if (country !== correctAnswer) {
+      playWrongAudio();
+    }
+  };
+
+  const playWrongAudio = () => {
+    const audio = new Audio(wrongAudio);
+    audio.play();
   };
 
   const handleNextQuestion = () => {
     setSelectedOption(null); // Reset selected option for the next question
+    setCurrentQuestionIndex(currentQuestionIndex + 1); // Move to the next question
   };
 
   const handlePreviousQuestion = () => {
     setSelectedOption(null); // Reset selected option when going back
+    setCurrentQuestionIndex(currentQuestionIndex - 1); // Move to the previous question
   };
+
+  const currentQuestion = optionsData[currentQuestionIndex];
+
+  // ... rest of the component code
+
+
 
   return (
     <div className="card">
@@ -31,6 +52,13 @@ export default function App() {
         <p className="card-text">
           <img src={require('./images/R.png')} alt="Flag R" className="flag-img" />
         </p>
+
+        {currentQuestion && (
+          <div>
+            <p className="mb-2">
+              Question {currentQuestionIndex + 1}/{optionsData.length}: {currentQuestion.question}
+            </p>
+        
         {optionsData.map((option, index) => (
           <div key={index} className="inline-buttons d-inline-block text-center mt-3">
             <button
@@ -55,13 +83,16 @@ export default function App() {
           </div>
         ))}
         <div className="button-group mt-3 d-flex justify-content-between">
-          <button className={`btn btn-primary ${isFirstQuestion ? 'disabled' : ''}`} onClick={handlePreviousQuestion}>
-            Back
-          </button>
-          <button className="btn btn-primary" onClick={handleNextQuestion}>
-            Next
-          </button>
-        </div>
+              <button
+                className={`btn btn-primary ${isFirstQuestion ? 'disabled' : ''}`}
+                onClick={handlePreviousQuestion}
+              >
+                Back
+              </button>
+              <button className="btn btn-primary" onClick={handleNextQuestion}>
+                Next
+              </button>
+            </div>
         {/* Display feedback messages */}
         {isWrongSelection && (
           <p className={`answer-message ${isWrongSelection ? 'wrong-answer-message' : 'correct-answer-message'}`}>
@@ -74,6 +105,8 @@ export default function App() {
           <p className={`answer-message correct-answer-message ${isWrongSelection ? 'popup' : ''}`}>
             Congratulations! You selected the correct answer: {correctAnswer}.
           </p>
+        )}
+      </div>
         )}
       </div>
     </div>
